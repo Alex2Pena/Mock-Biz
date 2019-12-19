bb795ad59f8d649f65ed011944b26d7d01c50dae
Alex2Pena <attn.Alex.P@gmail.com>
2019-12-19 10:04:52 -0800

    Fixed JS

--- js/add-content.js
+++ js/add-content.js
@@ -1,8 +1,14 @@
-var computer = prompt ("Would you like to purchase a mac or pc")
+var computer = prompt ("Would you like to purchase a mac or pc");
 
-if computer == mac { alert ("Awesome")
+if(computer === "mac") {
+    alert("Awesome");
 } 
-else if computer == pc { alert (Ewwwwwwww)
+else if(computer === "pc") {
+    alert("Ewwwwwwww");
 }
-else { confirm ("Can't decide?")
+else {
+    alert("Can't decide?")
 }
+
+var answer = confirm("Are you ready to buy?");
+console.log(answer)
